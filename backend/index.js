52fda70a461ea11b86ba320e8743624dfc19e224
AlejandroRugeles <93922279+AlejandroRugeles@users.noreply.github.com>
2021-11-18 21:52:35 -0500

    Update
    Update of backend code
    
    Co-Authored-By: AuraStella <93923970+AuraStella@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,76 @@
-const getUsuarios = require("./src/db/crud.js");
+
+const dbD = require("./src/db/crud.js");
 //getUsuarios();
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+app.use(express.json()); // formato predeterminado para las peticiones (json)
 
-app.get('/get-Usuarios', function(req, res){
-    getUsuarios(function(arrayUsuarios){
-        res.json(arrayUsuarios);
+//Sirve para traer todos los usuarios
+app.get('/Users',function(req, res){
+    dbD.getUsers(function(arrayUsers){
+        res.json(arrayUsers);
     })
 })
 
-app.get('/Usuarios',function(req, res){
-    res.send('Bienvenidos al servidor de Del campo a tu casa!, esta es una lista de usuarios');
+//Sirve para traer los datos del un usuario en específico
+app.get('/Users/:id',function(req, res){
+    const uid = req.params.id;
+    dbD.getUser(uid, function(refDoc){
+        res.json(refDoc);
+    })
 })
 
-app.listen(port, () => { 
-    console.log("Running on port" + port);
+//Crear un usuario en la base de datos
+app.put('/experts',function(req, res){
+    const User = req.body;
+    console.log(req.params);
+    dbD.addUser(User, function(status){
+        if(status === 'success'){
+            res.status(201).json(status);
+        } else {
+            res.status(503).json(status);
+        }
+    })
 })
+
+//Actualizar totalmente un usuario en la base de datos 
+app.put('/Users/:id', function(req, res){
+    const uid = req.params.id;
+    const User = req.body;
+    dbD.updateUserTotally(uid, User, function(status){
+        res.json(status);
+    })
+})
+
+//Actualizar parcialmente un usuario en la base de datos 
+app.patch('/Users/:id', function(req, res){
+    const uid = req.params.id;
+    const User = req.body;
+    dbD.updateUserPartially(uid, User, function(status){
+        res.json(status);
+    })
+})
+
+//Borrar usuario de la DB
+app.delete('/Users/:id', function(req, res) {
+    const uid = req.params.id;
+    dbD.deleteUser(uid, function(status){
+        res.json(status);
+    })
+})
+
+app.listen(port, () => {
+    console.log("Running on port " + port);
+})
+
+/* 
+// Traer un experto por ubicacion
+app.get('/Users/search/:location', function(req, res){
+    const location = req.params.location;
+    dbE.searchExpert(location, function(refDoc){
+        res.json(refDoc);
+    })
+}) */
--- backend/src/db/crud.js
+++ backend/src/db/crud.js
@@ -4,20 +4,138 @@ const db = require('./firebase.js');
 
 //Get all Usuarios
 
-function getUsuarios(callback) {
-    db.collection("Usuarios").get().then((docs) => {
-        var arrayUsuarios = []
-        docs.forEach((user) => {
+function getUsers(callback) {
+    return db.collection("Users").get().then((docs) => {
+        var arrayUsers = []
+        docs.forEach((User) => {
             //console.log(user.data());
-            arrayUsuarios.push(user.data());
+            const obj = User.data();
+            obj.uid = User.id; 
+            arrayUsers.push(obj);
 
         })
         //cuando llegamos aca se debe enviar la respuesta al Get Request
-        callback(arrayUsuarios);
-    });
+        callback(arrayUsers);
+    }).catch((error) => {
+        callback(`Error to get users ${error}`);
+    })
+}
+ //Obtener un usuario específico
+function getUser(uid, callback) {
+    return db.collection("Users").doc(uid).get()
+    .then((refDoc) =>{
+        callback(refDoc.data())
+    })
 }
 
-module.exports = getUsuarios;
+//Crear un usuario
+function addUser(User, callback) {
+    return db.collection("Users").add(User)
+    ,then(() =>{
+        callback("Success");
+    })
+    .catch((error) => {
+        callback(`Error to get users ${error}`);
+    })
+}
 
+//Actualizar usuario
+function updateUserTotally(uid, User, callback){
+    return db.collection("Users").doc(uid).set(User)
+    .then(() => {
+        callback("Success");
+    })
+    .catch((error) => {
+        callback(`Error to get users ${error}`);
+    })
+}
+
+function updateUserPartially(uid, User, callback){
+    return db.collection ("Users").doc(uid).update(User)
+    .then(() =>{
+        callback("Success");
+    })
+    .catch((error) =>{
+        callback(`Error to get users ${error}`);
+    })
+}
+
+//Eliminar usuario
+function deleteUser(uid, callback){
+    return db.collection ("Users").doc(uid).delete()
+    .then(() =>{
+        callback("Success");
+    })
+    .catch((error) => {
+        callback(`Error to get users ${error}`);
+    })
+}
+
+//Buscar usuarios
+function searchUser(location, callback){
+    return db.collection ("Users").where("location", "==", location).get()
+    .then((refDoc) =>{
+        var arrayUsers = [];
+        refDoc.forEach(doc =>{
+            arrayUsers.push(doc.data());
+        })
+        callback(arrayUsers);
+    })
+    .catch((err) =>{
+        callback("Error to search user ", err)
+    })
+}
+
+module.exports = {
+    getUsers, 
+    getUser, 
+    addUser, 
+    updateUserPartially, 
+    updateUserTotally,
+    deleteUser,
+    searchUser
+}
+
+// PARA PROBAR LOS METODOS
+// correr node crudExperts.js
+
+/*
+// Obtener un doc, pasando un id
+getUser("EiCurHlCXhSvqRDi1QR",(result) =>{
+    console.log(result);
+})
+
+// Crear un nuevo documento
+const expert = {
+    "name": "Ciro",
+    "location": "Bucaramanga, Colombia",
+    "occupation": "Programador"
+}
+
+addExpert(expert, (status)=>{
+    console.log(status);
+})
+
+
+const expert = {
+    "location" : "Cartagena",
+    "occupation": "Artista"
+}
+
+// Actualizar totalmente un documento
+updateExpertTotally("Ellz8i4XbcwwlDhujAdO", expert, function(status){
+    console.log(status);
+})
+
+// Actualizar parcialmente un documento
+updateExpertPartially("Ellz8i4XbcwwlDhujAdO", expert, function(status){
+    console.log(status);
+})
+
+// Borrar documento usando el id
+deleteExpert("kH9pnrlHoFCuruwDv9a5", (status) =>{
+    console.log(status);
+})
+*/
 
 
